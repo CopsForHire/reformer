4ea782a571e9d7726b9a62c1976a1804527bd5cd
Joshua Kifer <joshua.kifer@gmail.com>
2016-07-20 16:52:59 -0700

    Some abstraction, added drop triggers, partial create triggers

--- src/reformer.js
+++ src/reformer.js
@@ -1,11 +1,18 @@
 import kua from 'kua'
 import * as fs from 'fs'
-import * as R from 'ramda'
+
+kua.initialize()
+
+const commands = [
+  'generate-create-tables',
+  'generate-drop-triggers',
+  'generate-triggers',
+]
 
 const usage = `Usage: form <command> [options]
 
 commands:
-  generate-create-tables
+  ${commands.join('\n  ')}
 
 options:
   --schema <filepath> *required*
@@ -23,37 +30,136 @@ const historyTable = `CREATE TABLE \`history\` (
 );`
 
 class Formation {
-  constructor() {
-    kua.initialize()
-    if (!['generateCreateTables',
-         ].includes(kua.task) ||
-        !kua.option.schema ||
-        !fs.existsSync(kua.option.schema)) {
-      this.usage()
-    }
-    const schema = kua.loadYaml(kua.option.schema)
-    this[kua.task](schema)
-  }
-
   usage() {
     process.stdout.write(usage)
     process.exit()
   }
 
-  generateCreateTables(schema) {
-    const lines = []
-    for (const table of Object.keys(schema.tables)) {
-      const tableName = kua.camelize(table)
-      const parts = []
-      const rules = {}
-      const fields = {}
-      for (const key of Object.keys(schema.tables[table])) {
+  constructor() {
+    if (!commands.includes(kua.dasherize(kua.task)) ||
+        !kua.option.schema ||
+        !fs.existsSync(kua.option.schema)) {
+      this.usage()
+    }
+    this.loadSchema()
+    this[kua.task]()
+  }
+
+  loadSchema() {
+    this.schema = kua.loadYaml(kua.option.schema)
+    this.schema.rules = {}
+    for (const table of Object.keys(this.schema.tables)) {
+      this.schema.rules[table] = {}
+      for (const key of Object.keys(this.schema.tables[table])) {
         if (key[0] === ':') {
-          rules[kua.camelize(key.substr(1))] = schema.tables[table][key]
-        } else {
-          fields[kua.camelize(key)] = schema.tables[table][key]
+          this.schema.rules[table][kua.camelize(key.substr(1))] =
+            this.schema.tables[table][key]
+          delete this.schema.tables[table][key]
         }
       }
+    }
+  }
+
+  iterateTables(fn) {
+    for (const table of Object.keys(this.schema.tables)) {
+      fn(kua.camelize(table), this.schema.tables[table], this.schema.rules[table])
+    }
+  }
+
+  generateDropTriggers() {
+    const lines = []
+    this.iterateTables((table, fields, rules) => {
+      if (!rules.preventTriggers) {
+        for (const it of [
+          'before_insert',
+          'after_insert',
+          'before_update',
+          'after_update',
+          'after_delete',
+        ]) {
+          lines.push(`DROP TRIGGER IF EXISTS ${it}_${table};`)
+        }
+      }
+    })
+    lines.push('')
+    const result = lines.join('\n')
+    if (kua.option.print) {
+      process.stdout.write(result)
+    }
+    return result
+  }
+
+  generateTriggers() {
+    const lines = []
+    lines.push('DELIMITER $$')
+    this.iterateTables((table, fields, rules) => {
+      if (!rules.preventTriggers) {
+        lines.push(kua.leftAlign(`
+          CREATE TRIGGER before_insert_#table before insert on \`${table}\`
+          FOR EACH ROW BEGIN
+          IF NOT (
+            NEW.id REGEXP
+            '[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}'
+          ) THEN
+          SIGNAL SQLSTATE '45000' SET message_text = "You must provide a UUID for the 'id' column";
+          END IF;
+          END $$
+        `))
+        lines.push(kua.leftAlign(`
+          CREATE TRIGGER after_insert_#table after insert on \`${table}\`
+          FOR EACH ROW BEGIN
+        `))
+        lines.push(kua.leftAlign(`
+        `))
+        lines.push(kua.leftAlign(`
+        `))
+        lines.push(kua.leftAlign(`
+        `))
+        lines.push(kua.leftAlign(`
+        `))
+      }
+    })
+    lines.push('')
+    const result = lines.join('\n')
+    if (kua.option.print) {
+      process.stdout.write(result)
+    }
+    return result
+  }
+
+//     for column, def of structure
+//       lines.push "INSERT history (`session`, `table`, `id`, `at`, `op`, `column`, `value`) VALUES (@session, '#table', NEW.id, NOW(6), 'c', '#column', CAST(NEW.`#column` AS CHAR));"
+//     lines.push 'END $$'
+//     lines.push """
+//       CREATE TRIGGER before_update_#table before update on `#table`
+//       FOR EACH ROW BEGIN
+//       SET NEW.updated = NOW(6);
+//       END $$
+//     """
+//     lines.push """
+//       CREATE TRIGGER after_update_#table after update on `#table`
+//       FOR EACH ROW BEGIN
+//     """
+//     for column, def of structure
+//       lines.push """
+//         IF NEW.`#column` <> OLD.`#column` THEN
+//         INSERT history (`session`, `table`, `id`, `at`, `op`, `column`, `value`) VALUES (@session, '#table', NEW.id, NOW(6), 'u', '#column', CAST(NEW.`#column` AS CHAR));
+//         END IF;
+//       """
+//     lines.push 'END $$'
+//     lines.push """
+//       CREATE TRIGGER after_delete_#table after delete on `#table`
+//       FOR EACH ROW BEGIN
+//       INSERT history (`session`, `table`, `id`, `at`, `op`) VALUES (@session, '#table', OLD.id, NOW(6), 'd');
+//       END $$
+//     """
+//   lines.push 'DELIMITER ;'
+//   info lines.join '\n'
+
+  generateCreateTables() {
+    const lines = []
+    this.iterateTables((table, fields, rules) => {
+      const parts = []
       const structure = {}
       if (!rules.preventStandardAdditions) {
         kua.extend(structure, {
@@ -65,10 +171,10 @@ class Formation {
         })
       }
       kua.extend(structure, fields)
-      lines.push(`CREATE TABLE \`${tableName}\``)
+      lines.push(`CREATE TABLE \`${table}\``)
       lines.push('(')
       const maxKeyLength =
-        R.mapAccum((a, b) => [R.max(a, b.length), a], 0, Object.keys(structure))[0]
+        kua.fp.mapAccum((a, b) => [kua.fp.max(a, b.length), a], 0, Object.keys(structure))[0]
       for (const column of Object.keys(structure)) {
         const def = structure[column]
         if (def.ref) {
@@ -143,7 +249,7 @@ class Formation {
       if (t1 && t2) {
         parts.push(`  KEY \`${t1}\` (\`${t1}\`)`)
         parts.push(`  KEY \`${t2}\` (\`${t2}\`)`)
-        parts.push(`  UNIQUE KEY \`${tableName}\` (\`${t1}\`, \`${t1}\`)`)
+        parts.push(`  UNIQUE KEY \`${table}\` (\`${t1}\`, \`${t1}\`)`)
       }
       for (const column of Object.keys(structure)) {
         const def = structure[column]
@@ -160,10 +266,10 @@ class Formation {
       for (const column of Object.keys(structure)) {
         const def = structure[column]
         if (def.type === 'geometry') {
-          lines.push(`ALTER TABLE \`${tableName}\` ADD SPATIAL INDEX(\`${column}\`);`)
+          lines.push(`ALTER TABLE \`${table}\` ADD SPATIAL INDEX(\`${column}\`);\n`)
         }
       }
-    }
+    })
     lines.push(historyTable)
     lines.push('')
     const result = lines.join('\n')
